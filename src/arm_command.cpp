ff772f28780b9b4830f58acaf575394253a9e8d3
SeungJun <lsjun4524@naver.com>
2024-01-17 16:07:23 +0900

    modifed: arm_command, arm_main

--- src/arm_command.cpp
+++ src/arm_command.cpp
@@ -1,72 +1,106 @@
-#include "func.cpp" 
+#include "func.cpp"
+#include <chrono>
+#include <memory>
+
 
 using namespace std;
 using namespace Eigen;
 
 int op_mode;
 
-int main(int argc, char **argv) {
-    rclcpp::init(argc, argv);
-    auto node = rclcpp::Node::make_shared("jointcmd_publishing_node");
+class JointCommandPublisher : public rclcpp::Node
+{
+public:
+    JointCommandPublisher() : Node("jointcmd_publishing_node")
+    {
+        arm_command_pub = this->create_publisher<std_msgs::msg::Float32MultiArray>("/rrbot/ArmCmd", 100);
 
-    auto arm_command_pub = node->create_publisher<std_msgs::msg::float32_multi_array>("/rrbot/ArmCmd", 100);
+        // messages
+        joint_command_msg = std::make_shared<std_msgs::msg::Float32MultiArray>();
 
-    std_msgs::msg::float32_multi_array joint_command_msg;
+        // set up timer with 100Hz rate
+        timer_ = this->create_wall_timer(
+            std::chrono::milliseconds(10),  // 100Hz
+            std::bind(&JointCommandPublisher::timerCallback, this));
 
-    rclcpp::Rate loop_rate(100);
-    rclcpp::spin_some(node);
-
-    while (rclcpp::ok()) {
-        cout << "Choose control mode (1: joint position / 2: EE pose): " << '\n';
-        cin >> op_mode;
-
-		if(op_mode == 1) // joint position input
-		{
-			cout << "Enter the target joint position : " << '\n';
-			cin >> th1 >> th2 >> th3;
-			if (abs(th1) > 360 or abs(th2) > 360 or abs(th3) > 360){
-				cout << "Out of Joint Configuration!" << '\n';
-				continue;
-			}
-			th_cmd[0] = th1*PI/180;
-			th_cmd[1] = th2*PI/180;
-			th_cmd[2] = th3*PI/180;
-		}
-		else if(op_mode == 2) // EE pose input
-		{
-			char s;
-			cout << "Enter the target position and the solution type (u for upper, l for lower) : " << '\n';
-			cin >> x >> y >> z >> s;
-			if(s == 'u')
-				up = true;
-			else if(s == 'l')
-				up = false;
-			else{
-				cout << "Wrong solution! Type it again." << '\n';
-			}
-
-			try{
-				Inverse_K(x, y, z, up, th_cmd);
-			}
-			catch(...){
-				cout << "Out of Joint Configuration!" << '\n';
-				continue;
-			}
-		}
-		else{
-			cout << "try again!\n\n";
-            continue;
-		}
-
-        joint_command_msg.data.clear();
-        for (int i = 0; i < DoF; i++) {
-            joint_command_msg.data.push_back(th_cmd[i]);
+        // initialize variables
+        op_mode = 0;
+        th1 = th2 = th3 = x = y = z = 0.0;
+        up = false;
+        for (int i = 0; i < DoF; ++i)
+        {
+            th_cmd[i] = 0.0;
         }
-        arm_command_pub->publish(joint_command_msg);
-
-        rclcpp::spin_some(node);
-        loop_rate.sleep();
     }
 
+private:
+    void timerCallback()
+    {
+        cout << "Choose control mode ( 1: joint position / 2: EE pose ) : " << '\n';
+        cin >> op_mode;
+
+        if (op_mode == 1) // joint position input
+        {
+            cout << "Enter the target joint position : " << '\n';
+            cin >> th1 >> th2 >> th3;
+            if (abs(th1) > 360 or abs(th2) > 360 or abs(th3) > 360)
+            {
+                cout << "Out of Joint Configuration!" << '\n';
+                return;
+            }
+            th_cmd[0] = th1 * PI / 180;
+            th_cmd[1] = th2 * PI / 180;
+            th_cmd[2] = th3 * PI / 180;
+        }
+        else if (op_mode == 2) // EE pose input
+        {
+            char s;
+            cout << "Enter the target position and the solution type (u for upper, l for lower) : " << '\n';
+            cin >> x >> y >> z >> s;
+            if (s == 'u')
+                up = true;
+            else if (s == 'l')
+                up = false;
+            else
+            {
+                cout << "Wrong solution! Type it again." << '\n';
+                return;
+            }
+
+            try
+            {
+                Inverse_K(x, y, z, up, th_cmd);
+            }
+            catch (...)
+            {
+                cout << "Out of Joint Configuration!" << '\n';
+                return;
+            }
+        }
+        else
+        {
+            cout << "Try again!\n\n";
+            return;
+        }
+
+        joint_command_msg->data.clear();
+        for (int i = 0; i < DoF; i++)
+        {
+            joint_command_msg->data.push_back(th_cmd[i]);
+        }
+        arm_command_pub->publish(*joint_command_msg);
+    }
+
+    rclcpp::Publisher<std_msgs::msg::Float32MultiArray>::SharedPtr arm_command_pub;
+    std::shared_ptr<std_msgs::msg::Float32MultiArray>joint_command_msg;
+    rclcpp::TimerBase::SharedPtr timer_;
+};
+
+int main(int argc, char **argv)
+{
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<JointCommandPublisher>();
+    rclcpp::spin(node);
+    rclcpp::shutdown();
     return 0;
 }
--- src/arm_main.cpp
+++ src/arm_main.cpp
@@ -1,94 +1,121 @@
-#include "func.cpp" 
+#include "func.cpp"
 
 using namespace std;
 using namespace Eigen;
 
-void msgCallbackP(const sensor_msgs::JointState::ConstPtr& msg)
+class JointPublishingNode : public rclcpp::Node
 {
-    for (int i = 0; i < DoF; i++)
+public:
+    JointPublishingNode() : Node("joint_publishing_node")
     {
-        th_act[i] = msg->position[i];
+        joint1_pub = this->create_publisher<std_msgs::msg::Float64>("/rrbot/joint1_position_controller/command", 10);
+        joint2_pub = this->create_publisher<std_msgs::msg::Float64>("/rrbot/joint2_position_controller/command", 10);
+        joint3_pub = this->create_publisher<std_msgs::msg::Float64>("/rrbot/joint3_position_controller/command", 10);
+
+        sub_joint_angle = this->create_subscription<sensor_msgs::msg::JointState>(
+            "/rrbot/joint_states", 100, std::bind(&JointPublishingNode::msgCallbackP, this, std::placeholders::_1));
+
+        sub_joint_cmd = this->create_subscription<std_msgs::msg::Float32MultiArray>(
+            "/rrbot/ArmCmd", 100, std::bind(&JointPublishingNode::msgCallbackArmCmd_sim, this, std::placeholders::_1));
+
+        // messages
+        joint1_msg = std_msgs::msg::Float64();
+        joint2_msg = std_msgs::msg::Float64();
+        joint3_msg = std_msgs::msg::Float64();
     }
-    first_callback = true;
-}
 
-void msgCallbackArmCmd_sim(const std_msgs::Float32MultiArray::ConstPtr& msg)
-{
-    for (int i = 0; i < DoF; i++)
-    {
-        th_cmd[i] = msg->data[i];
-    }
-    traj_init = true;
-}
+private:
+    rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr joint1_pub;
+    rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr joint2_pub;
+    rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr joint3_pub;
+    rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr sub_joint_angle;
+    rclcpp::Subscription<std_msgs::msg::Float32MultiArray>::SharedPtr sub_joint_cmd;
 
-int main(int argc, char **argv) {
-    rclcpp::init(argc, argv);
-    auto node = rclcpp::Node::make_shared("joint_publishing_node");
-
-    auto joint1_pub = node->create_publisher<std_msgs::msg::Float64>("/rrbot/joint1_position_controller/command", 10);
-    auto joint2_pub = node->create_publisher<std_msgs::msg::Float64>("/rrbot/joint2_position_controller/command", 10);
-    auto joint3_pub = node->create_publisher<std_msgs::msg::Float64>("/rrbot/joint3_position_controller/command", 10);
-    auto sub_joint_angle = node->create_subscription<sensor_msgs::JointState>("/rrbot/JointStates", 100, msgCallbackP);
-    auto sub_joint_cmd = node->create_subscription<std_msgs::msg::Float32MultiArray>("/rrbot/ArmCmd", 100, msgCallbackArmCmd_sim);
-
-    rclcpp::Rate loop_rate(1000);
-    rclcpp::spin_some(node);
-
-    // messages
     std_msgs::msg::Float64 joint1_msg, joint2_msg, joint3_msg;
 
-    while (rclcpp::ok()) {
-        Forward_K(th_act, T03);
-
-        if (traj_init == true) // set when cmd angle come in, trajectory generated
+    void msgCallbackP(const sensor_msgs::msg::JointState::SharedPtr msg)
+    {
+        for (int i = 0; i < DoF; i++)
         {
-            for (int i = 0; i < DoF; i++) {
-                th_ini[i] = th_act[i];
-            }
-            Traj_joint(th_ini, th_cmd, th_out);
-            traj_cnt = 0;
-            traj_init = false;
+            th_act[i] = msg->position[i];
         }
-        else if (traj_cnt < th_out.rows()) // this portion must not run when no cmd
-        {
-            for (int i = 0; i < DoF; i++) {
-                TargetPos[i] = th_out(traj_cnt, i);
-            }
-            traj_cnt++;
-        }
-        else
-        {
-            for (int i = 0; i < DoF; i++) {
-                TargetPos[i] = th_cmd[i];
-            }
-        }
-
-        // PID_controller(TargetPos, th_act, TargetTor);
-        // torque_msg.data.clear();
-        // for (int i = 0; i < DoF; i++){
-        //     torque_msg.data.push_back(TargetTor[i]);
-        // }
-
-        // torque_pub.publish(torque_msg);
-
-        if (first_callback == true) {
-            joint1_msg.data = TargetPos[0];
-            joint2_msg.data = TargetPos[1];
-            joint3_msg.data = TargetPos[2];
-
-            joint1_pub->publish(joint1_msg);
-            joint2_pub->publish(joint2_msg);
-            joint3_pub->publish(joint3_msg);
-
-            // cout.precision(3);
-            // cout << "Target Joint / Pose :" << TargetPos[0] * 180 / PI << setw(6) << TargetPos[1] * 180 / PI
-            //      << setw(6) << TargetPos[2] * 180 / PI << "  /  ";
-            // cout << T03(0, 3) << setw(6) << T03(1, 3) << setw(6) << T03(2, 3) << '\n';
-        }
-
-        loop_rate.sleep();
-        rclcpp::spin_some(node);
+        first_callback = true;
     }
 
+    void msgCallbackArmCmd_sim(const std_msgs::msg::Float32MultiArray::SharedPtr msg)
+    {
+        for (int i = 0; i < DoF; i++)
+        {
+            th_cmd[i] = msg->data[i];
+        }
+        traj_init = true;
+    }
+
+    void run()
+    {
+        while (rclcpp::ok())
+        {
+            Forward_K(th_act, T03);
+
+            if (traj_init == true) // set when cmd angle comes in, trajectory generated
+            {
+                for (int i = 0; i < DoF; i++)
+                {
+                    th_ini[i] = th_act[i];
+                }
+                Traj_joint(th_ini, th_cmd, th_out);
+                traj_cnt = 0;
+                traj_init = false;
+            }
+            else if (traj_cnt < th_out.rows()) // this portion must not run when no cmd
+            {
+                for (int i = 0; i < DoF; i++)
+                {
+                    TargetPos[i] = th_out(traj_cnt, i);
+                }
+                traj_cnt++;
+            }
+            else
+            {
+                for (int i = 0; i < DoF; i++)
+                {
+                    TargetPos[i] = th_cmd[i];
+                }
+            }
+
+            // PID_controller(TargetPos, th_act, TargetTor);
+            // torque_msg.data.clear();
+            // for (int i = 0; i < DoF; i++){
+            //     torque_msg.data.push_back(TargetTor[i]);
+            // }
+
+            // torque_pub.publish(torque_msg);
+
+            if (first_callback == true)
+            {
+                joint1_msg.data = TargetPos[0];
+                joint2_msg.data = TargetPos[1];
+                joint3_msg.data = TargetPos[2];
+
+                joint1_pub->publish(joint1_msg);
+                joint2_pub->publish(joint2_msg);
+                joint3_pub->publish(joint3_msg);
+            }
+
+            cout.precision(3);
+            // cout << "Target Joint / Pose :" << TargetPos[0] * 180 / PI << setw(6) << TargetPos[1] * 180 / PI << setw(6) << TargetPos[2] * 180 / PI << "  /  ";
+            // cout << T03(0, 3) << setw(6) << T03(1, 3) << setw(6) << T03(2, 3) << '\n';
+
+            rclcpp::spin_some(this->get_node_base_interface());
+        }
+    }
+};
+
+int main(int argc, char **argv)
+{
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<JointPublishingNode>();
+    // node->run();
+    rclcpp::shutdown();
     return 0;
 }
